68753538854f80669859dd75bf67a16a04477888
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2022-02-28 08:56:32 +0530

    Update config.py

--- test/config.py
+++ test/config.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 #Telegram Bot API token (ask BotFather):
-botToken = ''
+botToken = '5037876930:AAGGW-VsmqLRqRGwLoG__-xZdMpIldc7MRg'
 
 #Password for authentication in bot:
 botPassword = 'password'
 
 #Default data for ssh-connection:
-sshUser = ''
-sshPassword = ''
-sshHost = ''
+sshUser = 'Devil'
+sshPassword = 'Ravishka12345'
+sshHost = '20.213.117.98'
 sshHomeDirectory = '/'
 
 #Root permission. If 'True" - root allowed
-rootPermission = False
+rootPermission = True
 
 #Logging. If 'True" - logging ON
 logging = True
